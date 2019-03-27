dvyukov/linux

commit 1996e6ba3efce20a6a64c527024acbd484db2f9d

kunit: test: improve error messages for invalid kconfigs
Was trying to port an existing kunitconfig to the alpha branch and found
the error pretty opaque:

	$ ./tools/testing/kunit/kunit.py run --raw_output
	Regenerating .config ...
	ERROR:root:Provided Kconfig is not contained in validated .config!

Include the missing fields in the error message:

	$ ./tools/testing/kunit/kunit.py run --raw_output
	Regenerating .config ...
	ERROR:root:Provided Kconfig contains fields not in validated
	.config: CONFIG_KUNIT=y, CONFIG_KUNIT_TEST=y,
	CONFIG_KUNIT_EXAMPLE_TEST=y

Signed-off-by: Eric Chiang <ericchiang@google.com>
Change-Id: I04bf8cff499fd3e548a6a8da7ff3ea65e44a0161

tools/testing/kunit/kunit_kernel.py
@@ -106,7 +106,10 @@ class LinuxSourceTree(object):
 		validated_kconfig = kunit_config.Kconfig()
 		validated_kconfig.read_from_file(KCONFIG_PATH)
 		if not self._kconfig.is_subset_of(validated_kconfig):
-			message = 'Provided Kconfig is not contained in validated .config!'
+			missing = self._kconfig.entries() - validated_kconfig.entries()
+			message = 'Provided Kconfig contains fields not in validated .config: %s' % (
+				', '.join([str(e) for e in missing]),
+			)
 			logging.error(message)
 			return ConfigResult(ConfigStatus.FAILURE, message)
 		return ConfigResult(ConfigStatus.SUCCESS, 'Build config!')
@@ -136,7 +139,10 @@ class LinuxSourceTree(object):
 		used_kconfig = kunit_config.Kconfig()
 		used_kconfig.read_from_file(KCONFIG_PATH)
 		if not self._kconfig.is_subset_of(used_kconfig):
-			message = 'Provided Kconfig is not contained in final config!'
+			missing = self._kconfig.entries() - validated_kconfig.entries()
+			message = 'Provided Kconfig contains fields not in final config: %s' % (
+				', '.join([str(e) for e in missing]),
+                        )
 			logging.error(message)
 			return BuildResult(BuildStatus.FAILURE, message)
 		return BuildResult(BuildStatus.SUCCESS, 'Built kernel!')

commit f192c1881e0d58be78e30cb0fbc6c29968bc0c79

kunit: tool: fixed issue introduced by improve error messages for invalid kconfigs
1996e6ba3efce20a6a64c527024acbd484db2f9d ("kunit: test: improve error
messages for invalid kconfigs",
https://kunit.googlesource.com/linux/+/1996e6ba3efce20a6a64c527024acbd484db2f9d)
broke kunit_tool when trying to build a kernel with an invalid config.

Google-Bug-Id: 129365734
Change-Id: Ib23456a0a976b5da6d00586db7d6b534a3adbe78
Signed-off-by: Brendan Higgins <brendanhiggins@google.com>

tools/testing/kunit/kunit_kernel.py
@@ -79,6 +79,15 @@ class LinuxSourceTreeOperations(object):
 
 		return output
 
+def throw_error_if_not_subset(expected_superset: kunit_config.Kconfig,
+			      expected_subset: kunit_config.Kconfig) -> None:
+	if not expected_subset.is_subset_of(expected_superset):
+		missing = expected_subset.entries() - expected_superset.entries()
+		message = 'Provided Kconfig contains fields not in validated .config: %s' % (
+			', '.join([str(e) for e in missing]),
+		)
+		raise ConfigError(message)
+
 class LinuxSourceTree(object):
 	"""Represents a Linux kernel source tree with KUnit tests."""
 
@@ -102,16 +111,15 @@ class LinuxSourceTree(object):
 			self._ops.make_olddefconfig()
 		except ConfigError as e:
 			logging.error(e)
-			return ConfigResult(ConfigStatus.FAILURE, e.message)
+			return ConfigResult(ConfigStatus.FAILURE, str(e))
 		validated_kconfig = kunit_config.Kconfig()
 		validated_kconfig.read_from_file(KCONFIG_PATH)
-		if not self._kconfig.is_subset_of(validated_kconfig):
-			missing = self._kconfig.entries() - validated_kconfig.entries()
-			message = 'Provided Kconfig contains fields not in validated .config: %s' % (
-				', '.join([str(e) for e in missing]),
-			)
-			logging.error(message)
-			return ConfigResult(ConfigStatus.FAILURE, message)
+		try:
+			throw_error_if_not_subset(expected_subset=self._kconfig,
+						  expected_superset=validated_kconfig)
+		except ConfigError as e:
+			logging.error(e)
+			return ConfigResult(ConfigStatus.FAILURE, str(e))
 		return ConfigResult(ConfigStatus.SUCCESS, 'Build config!')
 
 	def build_reconfig(self):
@@ -138,13 +146,12 @@ class LinuxSourceTree(object):
 			return BuildResult(BuildStatus.FAILURE, e.message)
 		used_kconfig = kunit_config.Kconfig()
 		used_kconfig.read_from_file(KCONFIG_PATH)
-		if not self._kconfig.is_subset_of(used_kconfig):
-			missing = self._kconfig.entries() - validated_kconfig.entries()
-			message = 'Provided Kconfig contains fields not in final config: %s' % (
-				', '.join([str(e) for e in missing]),
-                        )
-			logging.error(message)
-			return BuildResult(BuildStatus.FAILURE, message)
+		try:
+			throw_error_if_not_subset(expected_subset=self._kconfig,
+						  expected_superset=used_kconfig)
+		except ConfigError as e:
+			logging.error(e)
+			return ConfigResult(ConfigStatus.FAILURE, str(e))
 		return BuildResult(BuildStatus.SUCCESS, 'Built kernel!')
 
 	def run_kernel(self, args=[], timeout=None):